32df5141b8e6a873678d99f22af592fc948d3108
Narmin <xxx@gmail.com>
2026-02-16 14:56:41 +0300

    new

--- first.py
+++ first.py
@@ -1,15 +1,14 @@
 arr = list()
-n = int(input("Enter number of elements: "))
+b = int(input("Enter number of elements: "))
 print("Enter elements:")
 i = 0
-while (i < n):
+while (i < b):
     tmp = int(input(""))
     arr.append(tmp)
     i += 1
 print("Array:")
 i = 0
-while (i < n):
+while (i < b):
     print(arr[i], end = " ")
     i += 1
-11
 print("")
