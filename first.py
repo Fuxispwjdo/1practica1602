e71a998e82c62d88e5b0bfdb2bf09ada534af505
Жолобов Дмитрий <dimazhol8967@gmail.com>
2026-02-16 15:01:40 +0300

    second

--- first.py
+++ first.py
@@ -1,15 +1,14 @@
 arr = list()
-n = int(input("Enter number of elements: "))
+l = int(input("Enter number of elements: "))
 print("Enter elements:")
 i = 0
-while (i < n):
+while (i < l):
     tmp = int(input(""))
     arr.append(tmp)
     i += 1
 print("Array:")
 i = 0
-while (i < n):
+while (i < l):
     print(arr[i], end = " ")
     i += 1
-11
 print("")
